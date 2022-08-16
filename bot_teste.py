d894a7c6e841f493b156f079c2b9ee6277952e55
Aprendiz de MG <98169313+xtklim@users.noreply.github.com>
2022-08-16 15:24:53 -0300

    Update bot_teste.py
    Teste

--- bot_teste.py
+++ bot_teste.py
@@ -13,7 +13,7 @@ def teste(msg):
 		h, m, s = map(int, msgs.split(':'))
 		msdecimal = ((h * 60 + m) * 60 + s) * 1000
 		mshex = hex(msdecimal)
-		bot.send_message(msg.chat.id, f'Resultado:\n\nms decimal: \`\`\`{msdecimal}\`\`\`\n\nms hex: {mshex}\n\nms Long: {str(mshex)}L', parse_mode='html')
+		bot.send_message(msg.chat.id, f'Resultado:\n\nms decimal: ```{msdecimal}```\n\nms hex: {mshex}\n\nms Long: {str(mshex)}L', parse_mode='markdown')
 	except:
 		bot.send_message(msg.chat.id, 'Nao seja um tolo digite o seu tempo corretamente kk\nExemplo: /milis 23:23:23')
 
