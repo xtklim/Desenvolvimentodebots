d00395b746992b33d9a05e52fcb4a9b2bf3018b0
Aprendiz de MG <98169313+xtklim@users.noreply.github.com>
2022-08-16 12:45:25 -0300

    Add files via upload

---
+++ bot_teste.py
@@ -0,0 +1,20 @@
+import telebot
+
+
+token ='5164260190:AAFEIyPuIrzE6NcPs5dBV5VX9MclQBsvhkQ'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['milis'])
+def teste(msg):
+	msgs = msg.text[6:]
+	print(msgs)
+	try:
+		h, m, s = map(int, msgs.split(':'))
+		msdecimal = ((h * 60 + m) * 60 + s) * 1000
+		mshex = hex(msdecimal)
+		bot.send_message(msg.chat.id, f'Resultado:\n\nms decimal: {msdecimal}\n\nms hex: {mshex}\n\nms Long: {str(mshex)}L')
+	except:
+		bot.send_message(msg.chat.id, 'Nao seja um tolo digite o seu tempo corretamente kk\nExemplo: /milis 23:23:23')
+
+bot.infinity_polling()
